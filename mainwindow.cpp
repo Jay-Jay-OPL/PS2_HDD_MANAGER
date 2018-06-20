25873f277065959022136952118ebc40f89c8f2c
leyed0 <orley1994@hotmail.com>
2018-06-20 02:13:24 -0300

    Add files via upload

---
+++ mainwindow.cpp
@@ -0,0 +1,18 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    char buf[256];
+    ui->setupUi(this);
+    chdir("C:\\");
+    ui->textEdit->setText(getcwd(buf, sizeof(buf)));
+    //ui->listWidget->addItem("teste");
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
---
+++ mainwindow.h
@@ -0,0 +1,31 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <QMainWindow>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+
+private slots:
+
+private:
+    Ui::MainWindow *ui;
+};
+
+#endif // MAINWINDOW_H
